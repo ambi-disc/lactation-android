f9d1a694ebaa383be6e53f4ddd3fb91f7072443d
Nikki Beesetti <nbeesett@purdue.edu>
2017-10-15 11:35:36 -0400

    removes setters and constructor

--- app/src/main/java/org/lactor/consultant/api/LoginReturn.java
+++ app/src/main/java/org/lactor/consultant/api/LoginReturn.java
@@ -11,39 +11,15 @@ public class LoginReturn {
         return success;
     }
 
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
     public String getAuthToken() {
         return authToken;
-    }
-
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
     }
 
     public String getTimeoutStamp() {
         return timeoutStamp;
     }
 
-    public void setTimeoutStamp(String timeoutStamp) {
-        this.timeoutStamp = timeoutStamp;
-    }
-
     public String getErrorMessage() {
         return errorMessage;
-    }
-
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
-    }
-
-    public LoginReturn(boolean success, String authToken, String timeoutStamp, String errorMessage) {
-
-        this.success = success;
-        this.authToken = authToken;
-        this.timeoutStamp = timeoutStamp;
-        this.errorMessage = errorMessage;
     }
 }
