c7b0060a53015de9c9c769b4215e5c776452a06f
Nikki Beesetti <nbeesett@purdue.edu>
2017-10-15 11:25:27 -0400

    deleting getters and setters

--- app/src/main/java/org/lactor/consultant/api/AccountRequest.java
+++ app/src/main/java/org/lactor/consultant/api/AccountRequest.java
@@ -6,46 +6,6 @@ public class AccountRequest {
     public String lastName;
     public String emailAddress;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getProfession() {
-        return profession;
-    }
-
-    public void setProfession(String profession) {
-        this.profession = profession;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public AccountRequest(String firstName, String lastName, String emailAddress, String profession, String password) {
 
         this.firstName = firstName;
