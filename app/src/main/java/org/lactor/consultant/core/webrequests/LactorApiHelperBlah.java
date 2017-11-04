96cb46397fa6b84f2779e866bca3f3c9749969b2
Matthew Page <matthewpagecs@gmail.com>
2017-11-04 15:56:50 -0400

    In between

--- app/src/main/java/org/lactor/consultant/core/webrequests/LactorApiHelperBlah.java
+++ app/src/main/java/org/lactor/consultant/core/webrequests/LactorApiHelperBlah.java
@@ -2,17 +2,17 @@ package org.lactor.consultant.core.webrequests;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class LactorApiHelper {
-    private static LactorApi lactorApi;
+public class LactorApiHelperBlah {
+    private static LactorApiBlah lactorApi;
     private static String apiUrl = "http://hypermoon.matthewpage.xyz:5000/";
 
-    public static LactorApi getInstance() {
+    public static LactorApiBlah getInstance() {
         if (lactorApi == null) {
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(apiUrl)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
-            lactorApi = retrofit.create(LactorApi.class);
+            lactorApi = retrofit.create(LactorApiBlah.class);
         }
 
         return lactorApi;
