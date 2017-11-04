1c723a3f927f0723c79f368fc7335689285e1d26
Matthew Page <matthewpagecs@gmail.com>
2017-11-04 15:50:13 -0400

    Implemented navigation drawer

---
+++ app/src/main/java/org/lactor/consultant/homepage/ui/DrawerListAdapter.java
@@ -0,0 +1,11 @@
+package org.lactor.consultant.homepage.ui;
+
+import android.widget.BaseAdapter;
+
+/**
+ * @Author Matthew Page
+ * @Date 11/4/17
+ */
+
+public class DrawerListAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/org/lactor/consultant/homepage/ui/NavItem.java
@@ -0,0 +1,9 @@
+package org.lactor.consultant.homepage.ui;
+
+/**
+ * @Author Matthew Page
+ * @Date 11/4/17
+ */
+
+public class NavItem {
+}
